bba6af88875dd698d020fb57021e7875740617e2
Megan McEwan <meganmcewan@Megans-MacBook-Pro.local>
2018-05-17 16:03:24 -0400

    project gallery, moved admin folder

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ class Projects extends Component {
     super(props);
     this.state = {};
   }
-/// will need to also fetch data from slug only so she can share a specific page. Each page should have a specific slug
+  /// will need to also fetch data from slug only so she can share a specific page. Each page should have a specific slug
 
   componentDidMount() {
     const id = this.props.location.state.id;
@@ -22,14 +22,51 @@ class Projects extends Component {
 
   displayProject = () => {
     return (
-      <div className="img-cover">
-        <img
-          src={
-            this.state.project.better_featured_image.media_details.sizes.large
-              .source_url
-          }
-          alt={this.state.project.better_featured_image.alt_text}
-        />
+      <div className="gallery">
+        <div className="gallery-block">
+        <div className='img-cover'>
+          <img
+              className="gallery-img gallery-img-large"
+              src={this.state.project.acf.image_1.url}
+              alt={this.state.project.acf.image_1.alt}
+            />
+        </div>
+          
+          <div className="gallery-block-small">
+            <img
+              className="gallery-img"
+              src={this.state.project.acf.image_2.url}
+              alt={this.state.project.acf.image_2.alt}
+            />
+            <img
+              className="gallery-img"
+              src={this.state.project.acf.image_3.url}
+              alt={this.state.project.acf.image_3.alt}
+            />
+          </div>
+        </div> 
+
+        <div className="project-info">Info about projects go here</div>
+
+        <div className="gallery-block">
+          <img
+            className="gallery-img gallery-img-large img-cover"
+            src={this.state.project.acf.image_4.url}
+            alt={this.state.project.acf.image_4.alt}
+          />
+          <div className="gallery-block-small">
+            <img
+              className="gallery-img"
+              src={this.state.project.acf.image_5.url}
+              alt={this.state.project.acf.image_5.alt}
+            />
+            <img
+              className="gallery-img"
+              src={this.state.project.acf.image_6.url}
+              alt={this.state.project.acf.image_6.alt}
+            />
+          </div>
+        </div>
       </div>
     );
   };
